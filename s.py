1fc938fa501ca9e7861b2a6e22bbde477e5c88d3
Mark Tween <111575110+monsterxz9@users.noreply.github.com>
2024-04-29 11:14:30 +0800

    Update s.py

--- s.py
+++ s.py
@@ -1,10 +1,10 @@
 import pandas as pd
 
 # 读取CSV文件，指定逗号作为分隔符
-df = pd.read_csv('BrowserHistory_2024_4_28.csv', delimiter=',')
+df = pd.read_csv('your_csv', delimiter=',')
 
 # 过滤包含 "bilibili" 的行
-bilibili_df = df[df['NavigatedToUrl'].str.contains('pornhub')]
+bilibili_df = df[df['NavigatedToUrl'].str.contains('bilibili')]
 
 # 将结果保存到新的 CSV 文件中
-bilibili_df.to_csv('pornhub_urls.csv', index=False, encoding='utf-16')
+bilibili_df.to_csv('bilibili_urls.csv', index=False, encoding='utf-16')
